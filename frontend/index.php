a3dd5981bdeec2d809b8c8c3b096ffb0641bc175
zuby88517 <88517@glr.nl>
2022-11-22 14:18:47 +0100

    demo_build

--- frontend/index.php
+++ frontend/index.php
@@ -9,11 +9,23 @@
 </head>
 <body>
     <header>
-        <nav></nav>
-    </header>
+        <nav>
+            
 
+        </nav>
+    </header>
+    
+    <div id="links">
+    <a href="index.html">Home</a>
+    <a href="">Create blog</a>
+    <a href="">Browse blogs</a>
+
+    <a href="">Login</a>
+    <a href="">Register</a>
+    </div>
+    <hr />
     <footer>
-        
+
     </footer>
 </body>
 </html>
