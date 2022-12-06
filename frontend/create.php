b730a59bf38e96c073d205f05ef1bffa0a3df27a
zuby88517 <88517@glr.nl>
2022-12-06 13:19:45 +0100

    Nav bar complete

---
+++ frontend/create.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Excursieblog</title>
+</head>
+<body>
+    <img id="logo" src="images/logo.png" alt="">
+    <div id="links">
+    
+        <a href="index.php">Home</a>
+        <a href="">Create blog</a>
+        <a href="">Browse blogs</a>
+
+        <a href="../backend/login.php">Login</a>
+        <a href="#">Register</a>
+    </div>
+    <hr />
+    <div id="body-container">
+
+
+    </div>
+    <footer>
+
+    </footer>
+</body>
+</html>
